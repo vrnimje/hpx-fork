ab07b70b3cfd5158c55b5bf36bcf8055bfe8f738
Vedant <vedantnimjed@gmail.com>
2023-12-17 11:14:18 +0530

    Test for cmake platforn again
    Signed-off-by: Vedant <vedantnimjed@gmail.com>

--- cmake/HPX_SetupHwloc.cmake
+++ cmake/HPX_SetupHwloc.cmake
@@ -21,8 +21,7 @@ if(NOT HPX_WITH_FETCH_HWLOC)
     )
   endif()
 else()
-  hpx_info("System: ${CMAKE_GENERATOR_PLATFORM}")
-  hpx_info("System: ${CMAKE_SIZEOF_VOID_P}")
+  hpx_info("System: ${CMAKE_GENERATOR}")
   set(HWLOC_VERSION "2.9")
   set(HWLOC_RELEASE "2.9.3")
   hpx_info(
@@ -60,7 +59,7 @@ else()
       )
     endif()
 
-  elseif("${CMAKE_SIZEOF_VOID_P}" STREQUAL "8")
+  elseif("${CMAKE_GENERATOR}" STREQUAL "Win64" OR ("CMAKE_SIZEOF_VOID_P" STREQUAL "8"))
     fetchcontent_declare(
       HWLoc
       URL https://download.open-mpi.org/release/hwloc/v${HWLOC_VERSION}/hwloc-win64-build-${HWLOC_RELEASE}.zip
