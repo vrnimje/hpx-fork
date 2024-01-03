f035c454f234fee64d599cd35c61382c2ccd2033
Vedant <vedantnimjed@gmail.com>
2024-01-03 23:02:12 +0530

    Populating by default in Windows
    Signed-off-by: Vedant <vedantnimjed@gmail.com>

--- cmake/HPX_SetupHwloc.cmake
+++ cmake/HPX_SetupHwloc.cmake
@@ -42,9 +42,7 @@ if(HPX_WITH_FETCH_HWLOC)
       URL https://download.open-mpi.org/release/hwloc/v${HWLOC_VERSION}/hwloc-win64-build-${HWLOC_RELEASE}.zip
       TLS_VERIFY true
     )
-    if(NOT HWLoc_POPULATED)
-      fetchcontent_populate(HWLoc)
-    endif()
+    fetchcontent_populate(HWLoc)
     set(HWLOC_ROOT
         "${CMAKE_BINARY_DIR}/_deps/hwloc-src"
         CACHE INTERNAL ""
@@ -65,9 +63,7 @@ if(HPX_WITH_FETCH_HWLOC)
       URL https://download.open-mpi.org/release/hwloc/v${HWLOC_VERSION}/hwloc-win32-build-${HWLOC_RELEASE}.zip
       TLS_VERIFY true
     )
-    if(NOT HWLoc_POPULATED)
-      fetchcontent_populate(HWLoc)
-    endif()
+    fetchcontent_populate(HWLoc)
     set(HWLOC_ROOT
         "${CMAKE_BINARY_DIR}/_deps/hwloc-src"
         CACHE INTERNAL ""
