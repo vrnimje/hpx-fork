5030d1703271440f8c6520c761dd6833a7f7a3de
Vedant <vedantnimjed@gmail.com>
2024-03-03 22:40:39 +0530

    Added initial plotting script for perftests
    Signed-off-by: Vedant <vedantnimjed@gmail.com>

---
+++ tools/perftests_plot.py
@@ -0,0 +1,32 @@
+import sys
+import json
+import matplotlib.pyplot as plt
+
+file_name = sys.argv[1]
+
+fptr = open(file_name, "r")
+contents = fptr.read()
+
+print(contents)
+
+# Create a json ob
+json_obj = json.loads(contents)
+
+test_names = []
+samples = []
+
+for test in json_obj["outputs"]:
+    test_names.append(test["name"] + "," + test["executor"])
+    samples.append(test["series"])
+
+fig = plt.figure()
+ax = fig.add_subplot()
+bp = ax.boxplot(samples)
+
+ax.set_xticklabels(test_names)
+
+plt.show()
+
+
+    
+    
