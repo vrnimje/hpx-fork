ca5111e3e76f38c120dc6cd68f26843e217c037a
Vedant <vedantnimjed@gmail.com>
2024-04-11 23:15:41 +0530

    Updated cmdline switching to local config, and refactored benchmarks accordingly
    Signed-off-by: Vedant <vedantnimjed@gmail.com>

--- libs/core/command_line_handling_local/src/command_line_handling_local.cpp
+++ libs/core/command_line_handling_local/src/command_line_handling_local.cpp
@@ -628,10 +628,6 @@ namespace hpx::local::detail {
         {
             print_config(ini_config);
         }
-        
-        if (vm.count("hpx:verbose_bench")) {
-            verbose_ = true;
-        }
 
         return true;
     }
