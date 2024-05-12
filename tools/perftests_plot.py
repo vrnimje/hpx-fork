e2d45693189b41f44ca674cb7ee78b523ff4c814
Vedant <vedantnimjed@gmail.com>
2024-05-13 01:35:13 +0530

    Testing PR comments
    Signed-off-by: Vedant <vedantnimjed@gmail.com>

--- tools/perftests_plot.py
+++ tools/perftests_plot.py
@@ -1,31 +1,33 @@
 import sys
-import subprocess
+# import subprocess
 import json
 import matplotlib.pyplot as plt
 
-if len(sys.argv) != 2:
-    if len(sys.argv) == 1:
-        print("No benchmark selected!")
-    else:
-        print("Too many arguments!")
-    print("Usage: python perftests_plot.py [path_to_benchmark_binary]")
+if len(sys.argv) != 3:
+    print("Usage: python perftests_plot.py [path_to_first_result.json] [path_to_second_result.json]")
 else:
     test_name = sys.argv[1]
-
-    contents = subprocess.run([test_name, "--detailed_bench"], capture_output=True)
-
-    json_obj = json.loads(contents.stdout.decode('utf-8'))
+    f1 = open(sys.argv[1], 'r')
+    f2 = open(sys.argv[2], 'r')
+    json_obj1 = json.loads(f1.read())
+    json_obj2 = json.loads(f2.read())
 
     test_names = []
     samples = []
 
-    for test in json_obj["outputs"]:
-        test_names.append(test["name"] + ",\n" + test["executor"])
-        samples.append(test["series"])
+    for test1, test2 in zip(json_obj1["outputs"], json_obj2["outputs"]):
+        if test1["name"] == test2["name"]:
+            test_names.append(test1["name"] + ",\n" + test1["executor"])
+            samples.append(test1["series"])
+            test_names.append(test2["name"] + ",\n" + test2["executor"])
+            samples.append(test2["series"])
+        else:
+            exit(1)
 
     fig = plt.figure(figsize=(20, 6))
     ax = fig.add_subplot()
     bp = ax.boxplot(samples, showfliers=False)
     plt.setp(ax.set_xticklabels(test_names), fontsize=7)
-    plt.show()
+    # plt.show()
+    plt.savefig("test.jpg")
     
